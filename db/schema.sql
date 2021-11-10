182880aaff5b8e8acba4896d10c6e3723abd5cb4
David Dyer <davidrossdyer@gmail.com>
2021-11-10 18:32:04 -0500

    all set up with primary/foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -4,33 +4,33 @@ CREATE DATABASE crew_db;
 USE crew_db;
 
 CREATE TABLE departments (
-  dept_id INT NOT NULL AUTO_INCREMENT,
-  dept_name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (dept_id)
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  dept_name VARCHAR(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE roles (
-  -- AUTO_INCREMENT wont work!!??
-  role_id INT NOT NULL,
-  role_name VARCHAR(30) NOT NULL,
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  role_name VARCHAR(30) UNIQUE NOT NULL,
   dept VARCHAR(30) NOT NULL,
-  salary INT NOT NULL
-  -- FOREIGN KEY (dept)
-  -- REFERENCES departments(dept_id)
-  -- ON DELETE SET NULL
+  dept_id INT,
+  salary INT NOT NULL,
+  FOREIGN KEY (dept_id)
+  REFERENCES departments(id)
+  ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
-  employee_id INT NOT NULL,
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_name VARCHAR(30) NOT NULL,
   dept VARCHAR(30),
+  dept_id INT,
   salary INT NOT NULL,
-  manager VARCHAR(30)
-  -- FOREIGN KEY (dept_id)
-  -- REFERENCES departments(dept_id)
-  -- ON DELETE SET NULL
+  manager VARCHAR(30),
+  FOREIGN KEY (dept_id)
+  REFERENCES departments(id)
+  ON DELETE SET NULL
 );
 
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const mysql = require("mysql2");
-const cTable = require("console.table");
+const mysql = require("mysql");
+// const cTable = require("console.table");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,6 +8,31 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Connected");
+});
+
+app.get("/createdb", (req, res) => {
+  let sql = "CREATE DATABASE crew_db";
+  db.query(sql, (err) => {
+    if (err) throw err;
+    res.send("Database Created");
+  });
+});
+
+db.query("SELECT * FROM departments", (err, rows) => {
+  if (err) throw err;
+  console.log("DEPARTMENTS\n===========\n");
+  console.log(rows);
+});
 
 // Starts server listening on port var
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...🚀`));
